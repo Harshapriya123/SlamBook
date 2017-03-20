d018ee82bee11b09e8ff3edeae4ff0ca1b3c2727
Karthik Rangappa <Karthik Rangappa>
2017-03-19 19:47:48 -0500

    integrating DynamoDB

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,8 @@
 var express = require('express')
-var router = express.Router()
+var router = express.Router();
+var AWS = require('aws-sdk');
+
+var ddb = AWS.DynamoDB();
 
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
@@ -10,6 +13,31 @@ router.use(function timeLog (req, res, next) {
 // define the SignUp route
 router.post('/', function (req, res) {
   res.send('Signing Up!!')
+  var item = {
+    'email': {'S': req.body.email},
+    'name': {'S': req.body.name},
+    'phone':{'S': req.body.phone},
+    'zip-code':{'S': req.body.zip-code}
+  };
+
+  ddb.putItem({
+    'TableName': 'StartupSignupsTable',
+    'Item': item,
+    'Expected': { email: { Exists: false } }
+  },
+     function(err, data){
+            if(err){
+              var returnStatus = 500;
+              if (err.code === 'ConditionalCheckFailedException') {
+                  returnStatus = 409;
+              }
+              res.status(returnStatus).end();
+              console.log('DDB Error: ' + err);
+            }else{
+                res.status(201).end();
+            }
+  });
+
 })
 
 // define the SignUp/Details route
